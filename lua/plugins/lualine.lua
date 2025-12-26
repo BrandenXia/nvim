649e23b1f1c50daeb80d3110417d7ee945f01e4c
BrandenXia <xxtbranden@outlook.com>
2025-12-26 21:57:01 +0800

    feat(lualine): consider lsp when looking for root

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,6 @@
 local function get_root_dir()
 	local cwd = vim.fn.getcwd()
-	local root = vim.fs.root(0, { ".git" })
+	local root = vim.lsp.buf.list_workspace_folders()[1] or vim.fs.root(0, { ".git" })
 	local name = vim.fs.basename(root)
 
 	if root == cwd then
