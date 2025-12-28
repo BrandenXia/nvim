1f642bb9d698420dc04b9851ccfefa12883d8c78
BrandenXia <xxtbranden@outlook.com>
2025-12-28 18:33:32 +0800

    fix(lsp): use snacks builtin lsp finder

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -74,11 +74,12 @@ return {
           -- stylua: ignore
           keys = {
             { "<leader>cl", function() Snacks.picker.lsp_config() end, desc = "Lsp Info" },
-            { "gd", vim.lsp.buf.definition, desc = "Goto Definition", has = "definition" },
-            { "gr", vim.lsp.buf.references, desc = "References", nowait = true },
-            { "gI", vim.lsp.buf.implementation, desc = "Goto Implementation" },
-            { "gy", vim.lsp.buf.type_definition, desc = "Goto T[y]pe Definition" },
-            { "gD", vim.lsp.buf.declaration, desc = "Goto Declaration" },
+            -- use snacks builtin lsp finder instead
+            -- { "gd", vim.lsp.buf.definition, desc = "Goto Definition", has = "definition" },
+            -- { "gr", vim.lsp.buf.references, desc = "References", nowait = true },
+            -- { "gI", vim.lsp.buf.implementation, desc = "Goto Implementation" },
+            -- { "gy", vim.lsp.buf.type_definition, desc = "Goto T[y]pe Definition" },
+            -- { "gD", vim.lsp.buf.declaration, desc = "Goto Declaration" },
             { "K", function() return vim.lsp.buf.hover() end, desc = "Hover" },
             { "gK", function() return vim.lsp.buf.signature_help() end, desc = "Signature Help", has = "signatureHelp" },
             { "<c-k>", function() return vim.lsp.buf.signature_help() end, mode = "i", desc = "Signature Help", has = "signatureHelp" },
