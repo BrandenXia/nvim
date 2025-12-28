47bcf2278ba2bf7696028663b9ae4fd8af1cf3f2
BrandenXia <xxtbranden@outlook.com>
2025-12-28 22:34:34 +0800

    perf(kitty): use `opts = {}` instead of `config = true`

--- lua/plugins/kitty.lua
+++ lua/plugins/kitty.lua
@@ -11,6 +11,6 @@ return {
     event = { "User KittyScrollbackLaunch" },
     -- version = '*', -- latest stable version, may have breaking changes if major version changed
     version = "^6.0.0", -- pin major version, include fixes and features that do not have breaking changes
-    config = true,
+    opts = {},
   },
 }
