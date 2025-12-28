317020f8b8cd0d86c683b4206da478b811f26786
BrandenXia <xxtbranden@outlook.com>
2025-12-28 19:15:58 +0800

    refactor: separate `get_root_dir` function to `utils.fs`

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,14 +1,4 @@
-local function get_root_dir()
-  local cwd = vim.fn.getcwd()
-  local root = vim.lsp.buf.list_workspace_folders()[1] or vim.fs.root(0, { ".git" })
-  local name = vim.fs.basename(root)
-
-  if root == cwd then
-    return false
-  else
-    return name
-  end
-end
+local get_root_dir = require("utils.fs").get_root_dir
 
 local function root_dir_component()
   return {
