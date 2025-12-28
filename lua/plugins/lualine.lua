3f59326bfbdb0917b6a9db2b2e793675782ada52
BrandenXia <xxtbranden@outlook.com>
2025-12-28 19:17:35 +0800

    docs(lualine): add TODO in lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -55,6 +55,7 @@ return {
           },
           { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
           "filename",
+          -- TODO: implment equivalent of pretty_path in LazyVim
           -- { LazyVim.lualine.pretty_path() },
         },
         lualine_x = {
