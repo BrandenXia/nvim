934ea772cc7b8467c186809e74f655b2a0e43a07
BrandenXia <xxtbranden@outlook.com>
2025-12-28 23:57:20 +0800

    feat(langs): typst support

---
+++ lua/langs/typst.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = { "typst" },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tinymist = {
+          single_file_support = true, -- Fixes LSP attachment in non-Git directories
+          settings = {
+            formatterMode = "typstyle",
+          },
+        },
+      },
+    },
+  },
+  {
+    "chomosuke/typst-preview.nvim",
+    cmd = { "TypstPreview", "TypstPreviewToggle", "TypstPreviewUpdate" },
+    keys = {
+      {
+        "<leader>cp",
+        ft = "typst",
+        "<cmd>TypstPreviewToggle<cr>",
+        desc = "Toggle Typst Preview",
+      },
+    },
+    opts = {
+      dependencies_bin = {
+        tinymist = "tinymist",
+      },
+    },
+  },
+  {
+    "folke/ts-comments.nvim",
+    opts = {
+      lang = {
+        typst = { "// %s", "/* %s */" },
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        typst = { "typstyle", lsp_format = "prefer" },
+      },
+    },
+  },
+}
