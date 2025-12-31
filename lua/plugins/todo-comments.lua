449717869bc6230566192c20a7a55f338a401ade
BrandenXia <xxtbranden@outlook.com>
2025-12-31 09:47:32 +0800

    feat(todo-comments): Snacks.picker for todo-comments

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -2,6 +2,11 @@ return {
   {
     "folke/todo-comments.nvim",
     event = { "BufReadPost", "BufNewFile" },
+    -- stylua: ignore
+    keys = {
+      { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" },
+      { "<leader>sT", function () Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
+    },
     opts = {},
   },
   { "nvim-lua/plenary.nvim", lazy = true },
