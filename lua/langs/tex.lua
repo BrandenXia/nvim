5aa2344f08b79126e5f3fad290eaad0f014a6ee2
BrandenXia <xxtbranden@outlook.com>
2026-01-01 00:08:36 +0800

    feat(langs): Latex support

---
+++ lua/langs/tex.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = { "bibtex", "latex" },
+      disable = { "latex" },
+    },
+  },
+  {
+    "lervag/vimtex",
+    lazy = false, -- lazy-loading will disable inverse search
+    config = function()
+      vim.g.vimtex_view_method = "zathura"
+      vim.g.vimtex_view_automatic = 1
+      vim.g.vimtex_view_zathura_use_synctex = 0
+      vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
+      vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+    end,
+    keys = {
+      { "<localLeader>l", "", desc = "+vimtex", ft = "tex" },
+    },
+  },
+}
