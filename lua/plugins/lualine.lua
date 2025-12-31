19d82f3b33c88fca076f143a5662ce6d89386f29
BrandenXia <xxtbranden@outlook.com>
2025-12-31 20:26:12 +0800

    feat(lualine): pretty_path and trouble statusline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,17 +1,113 @@
-local get_root_dir = require("utils.fs").get_root_name
+local fs = require("utils.fs")
 
-local function root_dir_component()
+local M = {}
+
+function M.root_dir_component()
   return {
     function()
-      return "󱉭  " .. get_root_dir()
+      return "󱉭  " .. fs.get_root_name()
     end,
     cond = function()
-      return type(get_root_dir()) == "string"
+      return type(fs.get_root_name()) == "string"
     end,
     color = function()
       return { fg = Snacks.util.color("Special") }
     end,
   }
+end
+
+-- modified from LazyVim
+---@param component any
+---@param text string
+---@param hl_group? string
+---@return string
+function M.format(component, text, hl_group)
+  text = text:gsub("%%", "%%%%")
+  if not hl_group or hl_group == "" then
+    return text
+  end
+  ---@type table<string, string>
+  component.hl_cache = component.hl_cache or {}
+  local lualine_hl_group = component.hl_cache[hl_group]
+  if not lualine_hl_group then
+    local utils = require("lualine.utils.utils")
+    ---@type string[]
+    local gui = vim.tbl_filter(function(x)
+      return x
+    end, {
+      utils.extract_highlight_colors(hl_group, "bold") and "bold",
+      utils.extract_highlight_colors(hl_group, "italic") and "italic",
+    })
+
+    lualine_hl_group = component:create_hl({
+      fg = utils.extract_highlight_colors(hl_group, "fg"),
+      gui = #gui > 0 and table.concat(gui, ",") or nil,
+    }, "LV_" .. hl_group) --[[@as string]]
+    component.hl_cache[hl_group] = lualine_hl_group
+  end
+  return component:format_hl(lualine_hl_group) .. text .. component:get_default_hl()
+end
+
+---@param opts? {relative: "cwd"|"root", modified_hl: string?, directory_hl: string?, filename_hl: string?, modified_sign: string?, readonly_icon: string?, length: number?}
+function M.pretty_path(opts)
+  opts = vim.tbl_extend("force", {
+    relative = "cwd",
+    modified_hl = "MatchParen",
+    directory_hl = "",
+    filename_hl = "Bold",
+    modified_sign = "",
+    readonly_icon = " 󰌾 ",
+    length = 3,
+  }, opts or {})
+
+  return function(self)
+    local path = vim.fn.expand("%:p") --[[@as string]]
+
+    if path == "" then
+      return ""
+    end
+
+    local root = fs.get_root_dir()
+    local cwd = vim.fn.getcwd()
+
+    -- original path is preserved to provide user with expected result of pretty_path, not a normalized one,
+    -- which might be confusing
+    local norm_path = path
+
+    if opts.relative == "cwd" and norm_path:find(cwd, 1, true) == 1 then
+      path = path:sub(#cwd + 2)
+    elseif norm_path:find(root, 1, true) == 1 then
+      path = path:sub(#root + 2)
+    end
+
+    local sep = package.config:sub(1, 1)
+    local parts = vim.split(path, "[\\/]")
+
+    if opts.length == 0 then
+      parts = parts
+    elseif #parts > opts.length then
+      parts = { parts[1], "…", unpack(parts, #parts - opts.length + 2, #parts) }
+    end
+
+    if opts.modified_hl and vim.bo.modified then
+      parts[#parts] = parts[#parts] .. opts.modified_sign
+      parts[#parts] = M.format(self, parts[#parts], opts.modified_hl)
+    else
+      parts[#parts] = M.format(self, parts[#parts], opts.filename_hl)
+    end
+
+    local dir = ""
+    if #parts > 1 then
+      dir = table.concat({ unpack(parts, 1, #parts - 1) }, sep)
+      dir = M.format(self, dir .. sep, opts.directory_hl)
+    end
+
+    local readonly = ""
+    if vim.bo.readonly then
+      readonly = M.format(self, opts.readonly_icon, opts.modified_hl)
+    end
+    return dir .. parts[#parts] .. readonly
+  end
 end
 
 return {
@@ -30,6 +126,14 @@ return {
   opts = function()
     vim.o.laststatus = 3
     local icons = require("config.icons")
+    local symbols = require("trouble").statusline({
+      mode = "symbols",
+      groups = {},
+      title = false,
+      filter = { range = true },
+      format = "{kind_icon}{symbol.name:Normal}",
+      hl_group = "lualine_c_normal",
+    })
 
     return {
       options = {
@@ -43,7 +147,7 @@ return {
         lualine_a = { "mode" },
         lualine_b = { { "branch", padding = 2 } },
         lualine_c = {
-          root_dir_component(),
+          M.root_dir_component(),
           {
             "diagnostics",
             symbols = {
@@ -53,10 +157,12 @@ return {
               hint = icons.diagnostics.Hint,
             },
           },
-          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-          "filename",
-          -- TODO: implment equivalent of pretty_path in LazyVim
-          -- { LazyVim.lualine.pretty_path() },
+          { "filetype", icon_only = true, padding = { left = 1, right = 0 } },
+          { M.pretty_path(), separator = { right = "›" } },
+          {
+            symbols and symbols.get,
+            cond = symbols and symbols.has(),
+          },
         },
         lualine_x = {
           "filetype",
